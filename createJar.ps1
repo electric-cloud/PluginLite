9188e149806073e759fbfdfcd6f754bd493db084
Greg Maxey <gmaxey@electric-cloud.com>
2016-07-08 11:18:57 -0400

    Clean up console output

--- createJar.ps1
+++ createJar.ps1
@@ -22,23 +22,25 @@ function Add-Zip
 
         #$files = Get-ChildItem -Path $srcdir | where{! $_.PSIsContainer}
 
+	write-host "Zipping up directories"
     foreach($file in $input)
     {
-            write-host $file.FullName
-                        $zipPackage.CopyHere($file.FullName)
-                        while($zipPackage.Items().Item($file.name) -eq $null){
-                                Start-sleep -seconds 1
-                        }
-            #Start-sleep -milliseconds 500
+		write-host $file.FullName
+			$zipPackage.CopyHere($file.FullName)
+			while($zipPackage.Items().Item($file.name) -eq $null){
+					Start-sleep -seconds 1
+			}
     }
 }
 
 # Update project.xml with ec_setup.pl
+write-host "Updating project.xml with ec_setup.pl"
 $ec_setup = Get-Content ec_setup.pl
 $a = Select-Xml -Path .\META-INF\project.xml -XPath '//value[../propertyName/text() = "ec_setup"]'
 $a.Node.'#text'=$ec_setup -join "`n"
 $a.Node.OwnerDocument.Save($a.Path)
 
+write-host "Updating plugin.xml with key, version, label, description"
 # Update plugin.xml with key, version, label, description
 $b = Select-Xml -Path .\META-INF\plugin.xml -XPath '//key'
 $b.Node.'#text' = $pluginKey
@@ -56,14 +58,20 @@ $b = Select-Xml -Path .\META-INF\plugin.xml -XPath '//description'
 $b.Node.'#text' = $description
 $b.Node.OwnerDocument.Save($b.Path)
 
+write-host "Removing old zip and jar files"
 del "${pluginKey}.zip" -ErrorAction SilentlyContinue
 del "${pluginKey}.jar" -ErrorAction SilentlyContinue
 dir . | Add-Zip "${pluginKey}.zip"
 Move-Item "${pluginKey}.zip" "${pluginKey}.jar"
 
+write-host "Demoting old plugin"
 ectool promotePlugin "$pluginName" --promoted false
+write-host "Uninstalling old plugin"
 ectool uninstallPlugin "$pluginName"
+write-host "Installing new plugin"
 ectool installPlugin "${pluginKey}.jar"
+write-host "Promoting new plugin"
 ectool promotePlugin "$pluginName"
 
-type $Env:TEMP/log.txt
+write-host "Output from ec_setup.pl promoting plugin:"
+type $Env:TEMP/ec_setup.log
