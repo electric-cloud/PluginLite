33c18e01d258a4c03f7652823caec3304dbcc097
Greg Maxey <gmaxey@electric-cloud.com>
2016-07-14 17:37:28 -0400

    Remove pickList property

--- dsl/demote.groovy
+++ dsl/demote.groovy
@@ -2,4 +2,6 @@ def pluginName = args.pluginName
 def pluginKey = getProject("/plugins/$pluginName/project").pluginKey
 def pluginDir = getProperty("/server/settings/pluginsDirectory").value + "/" + pluginName
 
+deleteProperty propertyName: "/server/ec_customEditors/pickerStep/$pluginKey - Sample Procedure"
+
 return "Demoting plugin"
