885f3965b8dbc2babf8f2f264193b44e6308929f
Greg Maxey <gmaxey@electric-cloud.com>
2016-07-14 17:37:46 -0400

    Move pickList property to the procedure body

--- dsl/promote.groovy
+++ dsl/promote.groovy
@@ -4,29 +4,30 @@ def pluginKey = getProject("/plugins/$pluginName/project").pluginKey
 def pluginDir = getProperty("/server/settings/pluginsDirectory").value + "/" + pluginName
 // END Variables
 
-// Server level property that exposes plugin procedures to pipeline tasks,
-// application processes and component processes (pick lists)
-property "/server/ec_customEditors/pickerStep/$pluginKey - Sample Procedure", value:
-"""\
-<step>
-	<project>/plugins/$pluginKey/project</project>
-	<procedure>Sample Procedure</procedure>
-	<category>Utility</category>
-</step>
-""".stripIndent()
-// TIP: Remove this property in demote.groovy
-// END property
-
 // Sample plugin project content.  pluginName can be replaced by a name
 // to create a non-plugin project
 project pluginName,{
 	
 	// Make this plugin visible in all contexts
 	property "ec_visibility", value: "all" // Legal values: pickListOnly, hidden, all
-	property "pluginBaseName", value: pluginKey
-	property "pluginDir", value: pluginDir
 	
 	procedure "Sample Procedure",{
+	
+		// Server level property that exposes this procedure for use in pipeline tasks,
+		// application processes and component processes (pick lists). Note that
+		// procedureName an intrinsic variable for the procedure closure
+		property "/server/ec_customEditors/pickerStep/$pluginKey - $procedureName",
+			value:
+				"""\
+					<step>
+						<project>/plugins/$pluginKey/project</project>
+						<procedure>$procedureName</procedure>
+						<category>Utility</category>
+					</step>
+				""".stripIndent()
+		// TIP: Remove this property in demote.groovy
+		// END property	
+	
 		step "Hello", shell: "ec-perl",
 			// Get step content from a file in this plugin directory
 			command: new File(pluginDir + "/dsl/steps/Hello.pl").text
