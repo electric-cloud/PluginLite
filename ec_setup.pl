18f958f8458aeab2c52f0318df24965033951be9
Greg Maxey <gmaxey@electric-cloud.com>
2016-09-08 13:48:00 -0400

    Laurent's fixes

--- ec_setup.pl
+++ ec_setup.pl
@@ -1,18 +1,16 @@
-# ec_setup.pl
 use Cwd;
 use File::Spec;
 
 my $dir = getcwd;
 my $logfile ="";
 my $pluginDir;
-if(defined $ENV{'QUERY_STRING'}) { # Promotion through UI
-	$logfile = "$ENV{COMMANDER_PLUGINS}/$pluginName/ec_setup.log";
-	$pluginDir = "$ENV{ COMMANDER_PLUGINS}/$pluginName"
-	#$logfile = "../../$pluginName/ec_setup.log";
-	#$pluginDir = File::Spec->rel2abs( "../../$pluginName" );
+if(defined $ENV{QUERY_STRING}) { # Promotion through UI
+       $logfile   = $ENV{COMMANDER_PLUGINS} . "/$pluginName/ec_setup.log";
+       $pluginDir = $ENV{COMMANDER_PLUGINS} . "/$pluginName";
+
 } else {
-	$logfile = "$ENV{'TEMP'}/ec_setup.log";
-	$pluginDir = $dir;
+       $logfile   = $ENV{TEMP} . "/ec_setup.log";
+       $pluginDir = $dir;
 }
 $commander->setProperty("/plugins/$pluginName/project/pluginDir",{value=>$pluginDir});
 open(my $fh, '>', $logfile) or die "Could not open file '$logfile' $!";
@@ -22,19 +20,19 @@ print $fh "Current directory: $dir\n";
 # Evaluate promote.groovy or demote.groovy based on whether plugin is being promoted or demoted ($promoteAction)
 local $/ = undef;
 # If env variable QUERY_STRING exists:
-if(defined $ENV{'QUERY_STRING'}) { # Promotion through UI
-	open FILE, "../../$pluginName/dsl/$promoteAction.groovy" or die "Couldn't open file: $!";
+if(defined $ENV{QUERY_STRING}) { # Promotion through UI
+       open FILE, $ENV{COMMANDER_PLUGINS} . "/$pluginName/dsl/$promoteAction.groovy" or die "Couldn't open file: $!";
 } else {  # Promotion from the command line
-	open FILE, "dsl/$promoteAction.groovy" or die "Couldn't open file: $!";
+       open FILE, "dsl/$promoteAction.groovy" or die "Couldn't open file: $!";
 }
 my $dsl = <FILE>;
 close FILE;
 my $dslReponse = $commander->evalDsl($dsl,
-		{parameters=>qq(
-			{
-				"pluginName":"$pluginName"
-			}
-		)}
+              {parameters=>qq(
+                     {
+                           "pluginName":"$pluginName"
+                     }
+              )}
 )->findnodes_as_string("/");
 print $fh $dslReponse;
 
@@ -44,6 +42,6 @@ close $fh;
 open LOGFILE, $logfile or die "Couldn't open file: $!";
 my $logFileContent = <LOGFILE>;
 my $propertyResponse = $commander->setProperty("/plugins/$pluginName/project/ec_setup.log",
-			{value=>$logFileContent}
-	);
+                     {value=>$logFileContent}
+       );
 close LOGFILE;
