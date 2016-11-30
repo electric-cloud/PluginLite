1a43a09a73bb2f675a2104783b79f17fe00f573c
Laurent Rochette <lrochette@electric-cloud.com>
2016-11-30 16:13:45 -0500

    Add code to support installation through a step

--- ec_setup.pl
+++ ec_setup.pl
@@ -17,8 +17,9 @@ $logfile .= "Current directory: $dir\n";
 
 # Evaluate promote.groovy or demote.groovy based on whether plugin is being promoted or demoted ($promoteAction)
 local $/ = undef;
-# If env variable QUERY_STRING exists:
-if(defined $ENV{QUERY_STRING}) { # Promotion through UI
+
+#If env variable QUERY_STRING exists (UI installation) or we are in a step:
+if(defined $ENV{COMMANDER_JOBSTEPID} || defined $ENV{QUERY_STRING}) { 
        open FILE, $ENV{COMMANDER_PLUGINS} . "/$pluginName/dsl/$promoteAction.groovy" or die "Couldn't open file: $!";
 } else {  # Promotion from the command line
        open FILE, "dsl/$promoteAction.groovy" or die "Couldn't open file: $!";
